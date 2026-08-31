d02509c8c9a9c05dea3a670322dafddaf81b3245
Supreme-Prime <calebnjogu665@gmail.com>
2026-08-31 14:12:21 +0300

    Modify HTTP server to use MENU_IMAGE from config
    Updated the HTTP server to replace a placeholder with the actual MENU_IMAGE from the config.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ global.ytdl = ytdl;
 global.sharp = sharp;
 global.ffmpegPath = ffmpeg.path;
 
-// HTTP Server for Render
+// HTTP Server (Dynamic HTML)
 const server = http.createServer((req, res) => {
     const filePath = path.join(__dirname, 'public', 'index.html');
     fs.readFile(filePath, (err, data) => {
@@ -27,8 +27,10 @@ const server = http.createServer((req, res) => {
             res.writeHead(404, { 'Content-Type': 'text/plain' });
             res.end('Page not found');
         } else {
+            // Replace the placeholder with the actual MENU_IMAGE from config
+            const html = data.toString().replace(/{{MENU_IMAGE}}/g, config.MENU_IMAGE);
             res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
+            res.end(html);
         }
     });
 });
