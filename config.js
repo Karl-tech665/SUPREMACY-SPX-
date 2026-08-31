0e941267019cc3d4e3fadc909181cb81934864a7
Supreme-Prime <calebnjogu665@gmail.com>
2026-08-31 14:01:26 +0300

    Modify group join and follow channel configurations
    Updated AUTO_JOIN_GROUP and AUTO_FOLLOW_CHANNEL settings.

--- config.js
+++ config.js
@@ -3,12 +3,17 @@ module.exports = {
     OWNER_NAME: "༒♔𝕾𝖚𝖕𝖗𝖊𝖒𝖊 𝕻𝖗𝖎𝖒𝖊♔༒",
     OWNER_NUMBER: "254725977353",
     PREFIX: ".",
-    MENU_IMAGE: "https://i.imgur.com/90O9xAC.jpeg",
+    MENU_IMAGE: "https://i.imgur.com/90O9xAC.jpeg", // Your dynamic pic
     PROXY: {
         LINK: "https://wa.me/proxy?host=de.greenapi.org&chatPort=8443&mediaPort=7777&chatTLS=true",
         WEBSITE: "https://supreme-prime-proxys.vercel.app"
     },
-    AUTO_JOIN_GROUP: "", // Keep empty to avoid crashes/bans
-    AUTO_FOLLOW_CHANNEL: "", // Keep empty to avoid crashes/bans
+    // The invite code is just the ID at the end of the link
+    AUTO_JOIN_GROUP: "KuRigerELFNBFIyfLp9xJ5", 
+    // Converted to an array of JIDs for the robust autoFollowChannels function
+    AUTO_FOLLOW_CHANNEL: [
+        "0029Vb8sraAH5JM5WTpz0X0z@newsletter", 
+        "0029Vb84TR9IXnltkyhYEC3R@newsletter"
+    ],
     SESSION_DIR: process.env.SESSION_DIR || "./session"
 };
